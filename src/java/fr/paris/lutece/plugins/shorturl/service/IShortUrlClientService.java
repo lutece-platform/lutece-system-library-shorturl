aa4558704cfafebee26ed5d10664e54a27575797
FrancoisEricMerlin <francoiseric.merlin@paris.fr>
2019-04-01 16:18:57 +0200

    LSU-1:Init Project

--- src/java/fr/paris/lutece/plugins/shorturl/service/IShortUrlClientService.java
+++ src/java/fr/paris/lutece/plugins/shorturl/service/IShortUrlClientService.java
@@ -1,12 +1,14 @@
 package fr.paris.lutece.plugins.shorturl.service;
 
+import javax.servlet.http.HttpServletRequest;
+
 public interface IShortUrlClientService
 {
-    ShortUrlJsonResponse createShortener( String strUrl );
+    ShortUrlJsonResponse createShortener( String strUrl,HttpServletRequest request);
 
-    ShortUrlJsonResponse createShortener( String strUrl, boolean bUseOnce );
+    ShortUrlJsonResponse createShortener( String strUrl, boolean bUseOnce,HttpServletRequest request );
 
-    boolean deleteShortener( String strKey );
+    boolean deleteShortener( String strKey,HttpServletRequest request );
     
-    void setServerUrl( String _strServerUrl );
+    void setServerBaseUrl( String _strServerUrl );
 }
--- src/java/fr/paris/lutece/plugins/shorturl/service/ShortUrlClientService.java
+++ src/java/fr/paris/lutece/plugins/shorturl/service/ShortUrlClientService.java
@@ -3,6 +3,8 @@ package fr.paris.lutece.plugins.shorturl.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.lang.StringUtils;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -10,18 +12,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import fr.paris.lutece.portal.service.util.AppLogService;
+import fr.paris.lutece.portal.service.util.AppPathService;
 import fr.paris.lutece.util.httpaccess.HttpAccess;
 import fr.paris.lutece.util.json.JsonResponse;
 
 public class ShortUrlClientService implements IShortUrlClientService
 {
     private static ObjectMapper _mapper;
-    private String _strServerUrl=null;
+    private String _strShortServerUrl=null;
     
     private static final String STATUS_OK = "OK";
+    
     private static final String ACTION_DO_CREATE_WS = "do_create_ws";
     private static final String ACTION_DO_DELETE_WS = "do_delete_ws";
-
+    private static final String SHORT_XPAGE = "shorturl";
+    private static final String PARAMETER_PAGE = "page";
     private static final String PARAMETER_URL = "url";
     private static final String PARAMETER_KEY = "key";
     private static final String PARAMETER_USE_ONCE = "use_once";
@@ -36,13 +41,13 @@ public class ShortUrlClientService implements IShortUrlClientService
         _mapper.enable( SerializationFeature.WRAP_ROOT_VALUE );
     }
      
-    public  ShortUrlJsonResponse createShortener(  String strUrl)
+    public  ShortUrlJsonResponse createShortener(  String strUrl,HttpServletRequest request)
     {
-        return createShortener( strUrl,false);
+        return createShortener( strUrl,false,request);
     }
 
     
-    public  ShortUrlJsonResponse createShortener(String strUrl ,boolean bUseOnce)
+    public  ShortUrlJsonResponse createShortener(String strUrl ,boolean bUseOnce,HttpServletRequest request)
     {
 
         ShortUrlJsonResponse shortUrlJsonResponse=null;
@@ -50,13 +55,17 @@ public class ShortUrlClientService implements IShortUrlClientService
         Map<String, String> mapParams = new HashMap<String, String>( );
         mapParams.put( PARAMETER_ACTION, ACTION_DO_CREATE_WS );
         mapParams.put( PARAMETER_URL, strUrl );
+        mapParams.put( PARAMETER_PAGE, SHORT_XPAGE );
+        
         mapParams.put( PARAMETER_USE_ONCE, Boolean.toString( bUseOnce ) );
+        
+        String serverUrl=StringUtils.isEmpty( _strShortServerUrl)?AppPathService.getBaseUrl( request):_strShortServerUrl;
         
         String strOutput = StringUtils.EMPTY;
 
         try
         {
-            strOutput = clientHttp.doPost( _strServerUrl, mapParams, null, null, null, null );
+            strOutput = clientHttp.doPost( serverUrl +"/jsp/site/Portal.jsp", mapParams, null, null, null, null );
             shortUrlJsonResponse = _mapper.readValue( strOutput, ShortUrlJsonResponse.class );
         }
         catch( Exception e )
@@ -70,20 +79,23 @@ public class ShortUrlClientService implements IShortUrlClientService
     }
 
 
-    public   boolean deleteShortener( String strKey )
+    public   boolean deleteShortener( String strKey,HttpServletRequest request )
     {
     
         HttpAccess clientHttp = new HttpAccess( );
         Map<String, String> mapParams = new HashMap<String, String>( );
         mapParams.put( PARAMETER_ACTION, ACTION_DO_DELETE_WS );
         mapParams.put( PARAMETER_KEY, strKey );
+        mapParams.put( PARAMETER_PAGE, SHORT_XPAGE );
+        
         JsonResponse deleteResponse=null;
         
         String strOutput = StringUtils.EMPTY;
-
+        String serverUrl=StringUtils.isEmpty( _strShortServerUrl)?AppPathService.getBaseUrl( request):_strShortServerUrl;
+        
         try
         {
-            strOutput = clientHttp.doPost( _strServerUrl, mapParams, null, null, null, null );
+            strOutput = clientHttp.doPost( serverUrl, mapParams, null, null, null, null );
             deleteResponse = _mapper.readValue( strOutput, JsonResponse.class );
         }
         catch( Exception e )
@@ -99,10 +111,12 @@ public class ShortUrlClientService implements IShortUrlClientService
 
 
 
-    public void setServerUrl( String _strServerUrl )
+    public void setServerBaseUrl( String _strServerUrl )
     {
-        this._strServerUrl = _strServerUrl;
+        this._strShortServerUrl = _strServerUrl;
     }
+    
+    
 
  
 
