7c2bad475b981b9c8eca77e9f151c3eb970dd563
FrancoisEricMerlin <francoiseric.merlin@paris.fr>
2019-04-10 14:17:20 +0200

    LSU-1:Format + License+ Xdoc

--- src/java/fr/paris/lutece/plugins/shorturl/service/IShortUrlClientService.java
+++ src/java/fr/paris/lutece/plugins/shorturl/service/IShortUrlClientService.java
@@ -1,14 +1,86 @@
+/*
+ * Copyright (c) 2002-2019, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
 package fr.paris.lutece.plugins.shorturl.service;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * 
+ * IShortUrlClientService
+ *
+ */
 public interface IShortUrlClientService
 {
+    /**
+     * create shortener Url
+     * 
+     * @param strUrl
+     *            the url
+     * @param request
+     *            the request
+     * @return a short url
+     */
     ShortUrlJsonResponse createShortener( String strUrl, HttpServletRequest request );
 
+    /**
+     * create shortener Url
+     * 
+     * @param strUrl
+     *            the url
+     * @param bUseOnce
+     *            true if the shortUrl can be used just one time
+     * @param request
+     *            the request
+     * @return
+     */
     ShortUrlJsonResponse createShortener( String strUrl, boolean bUseOnce, HttpServletRequest request );
 
+    /**
+     * Delete short url using short url key
+     * 
+     * @param strKey
+     *            the sorturl key
+     * @param request
+     *            the request
+     * @return true if the url has been delete
+     */
     boolean deleteShortener( String strKey, HttpServletRequest request );
 
-    void setServerBaseUrl( String _strServerUrl );
+    /**
+     * set serverUrl
+     * 
+     * @param strServerUrl
+     */
+    void setServerBaseUrl( String serverUrl );
 }
--- src/java/fr/paris/lutece/plugins/shorturl/service/ShortUrlClientService.java
+++ src/java/fr/paris/lutece/plugins/shorturl/service/ShortUrlClientService.java
@@ -1,3 +1,36 @@
+/*
+ * Copyright (c) 2002-2019, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
 package fr.paris.lutece.plugins.shorturl.service;
 
 import java.util.HashMap;
@@ -9,13 +42,17 @@ import org.apache.commons.lang.StringUtils;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 import fr.paris.lutece.portal.service.util.AppLogService;
 import fr.paris.lutece.portal.service.util.AppPathService;
 import fr.paris.lutece.util.httpaccess.HttpAccess;
 import fr.paris.lutece.util.json.JsonResponse;
 
+/**
+ * 
+ * ShortUrlClientService
+ *
+ */
 public class ShortUrlClientService implements IShortUrlClientService
 {
     private static ObjectMapper _mapper;
@@ -40,11 +77,19 @@ public class ShortUrlClientService implements IShortUrlClientService
         // _mapper.enable( SerializationFeature.WRAP_ROOT_VALUE );
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public ShortUrlJsonResponse createShortener( String strUrl, HttpServletRequest request )
     {
         return createShortener( strUrl, false, request );
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public ShortUrlJsonResponse createShortener( String strUrl, boolean bUseOnce, HttpServletRequest request )
     {
 
@@ -76,6 +121,10 @@ public class ShortUrlClientService implements IShortUrlClientService
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public boolean deleteShortener( String strKey, HttpServletRequest request )
     {
 
@@ -105,6 +154,10 @@ public class ShortUrlClientService implements IShortUrlClientService
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void setServerBaseUrl( String _strServerUrl )
     {
         this._strShortServerUrl = _strServerUrl;
